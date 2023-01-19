f08c8bdd5bcab1169933e0c90ee9ae8ef90fb9bd
JRWinthenet <108890060+JRWinthenet@users.noreply.github.com>
2023-01-19 17:15:48 -0300

    Update s.py

--- s.py
+++ s.py
@@ -1,7 +1,6 @@
 import socket
 server = socket.socket() 
-ip = input("Insert ip here: ")
-server.bind((ip, 10020)) 
+server.bind(("", 10020))
 server.listen(4) 
 client_socket, client_address = server.accept()
 print(client_address, "has connected")
